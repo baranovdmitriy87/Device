32b4bb0b1d164a78882af5a354ac1a1ec984ae96
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-03-10 00:50:45 +0300

    Заканчивает реализацию хедера и начинает делать блок со слайдером

--- js/app.js
+++ js/app.js
@@ -1,16 +1,65 @@
 const btnDropdown = document.querySelector(".header-catalog-btn");
+const listWrapper = document.querySelector(".header-catalog-list-wrapper");
+const isActiveFeaturesLinks = document.querySelectorAll(".header-content-features-link");
+const showCatalogLinks = document.querySelectorAll(".header-catalog-link");
+// const navUserLink = document.querySelector(".nav-user-link span");
+// const authUser = document.querySelector(".nav-link-auth");
+// const authNoUser = document.querySelector(".nav-link-auth-active");
 
-// Показать подменю каталога товаров
+// const navUserShow = () => {
+
+//   navUserLink.addEventListener('click', (evt) => {
+//     evt.preventDefault();
+//     authUser.classList.remove('nav-link-auth');
+//     authNoUser.classList.add('nav-link-auth-active');
+//     console.log ('click')
+//   });
+// }
+
+// navUserShow();
+
+// Показать список товаров каталога
 const dropdownShow = () => {
-  btnDropdown.addEventListener("click", (event) => {
+  btnDropdown.addEventListener("click", () => {
     btnDropdown.classList.toggle("is-active");
-    const catalogProductList = btnDropdown.nextElementSibling;
     if (btnDropdown.classList.contains("is-active")) {
-      catalogProductList.style.maxHeight = catalogProductList.scrollHeight + "px";
+      listWrapper.classList.remove('closed')
+      listWrapper.classList.add('opened')
     } else {
-      catalogProductList.style.maxHeight = 0;
+      listWrapper.classList.add('closed')
+      listWrapper.classList.remove('opened')
     }
   });
 }
 
 dropdownShow();
+
+// Показать активный features элемент
+const featuresShow = () => {
+  isActiveFeaturesLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      isActiveFeaturesLinks.forEach((el) => {
+        el.classList.remove('is-active');
+      });
+      link.classList.add('is-active');
+    });
+  });
+}
+
+featuresShow();
+
+// Показать активный элемент в каталоге товаров
+const catalogProductsShow = () => {
+  showCatalogLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      showCatalogLinks.forEach((el) => {
+        el.classList.remove('is-active');
+      });
+      link.classList.add('is-active');
+    });
+  });
+}
+
+catalogProductsShow();
